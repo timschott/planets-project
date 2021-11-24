7a9eec128e3206e8e269f48b79565bfaa2a49fcf
timschott <tcs9pk@virginia.edu>
2021-11-24 11:42:36 -0800

    skeleton for data storage

--- planets/src/Homepage.js
+++ planets/src/Homepage.js
@@ -1,4 +1,26 @@
-function Homepage() {    
+import { useEffect, useState } from 'react';
+
+function Homepage() { 
+    
+    const [mercury, setMercury] = useState([]);
+    const [venus, setVenus] = useState([]);
+    const [earth, setEarth] = useState([]);
+    const [mars, setMars] = useState([]);
+    const [jupiter, setJupiter] = useState([]);
+    const [saturn, setSaturn] = useState([]);
+    const [uranus, setUranus] = useState([]);
+    const [neptune, setNeptune] = useState([]);
+    const [pluto, setPluto] = useState([]);
+
+    // passing an empty array as the second argument
+    // means this is only called on an initial render
+    useEffect(() => {
+        fetch('https://api.le-systeme-solaire.net/rest/bodies')
+        .then(results => results.json())
+        .then(data => {
+            console.log(data)
+        })
+    }, []);
 
     return (
         <div className = "homepage_contents">

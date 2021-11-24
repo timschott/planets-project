d5928902a1567e235375374af15b46cc2e300d64
timschott <tcs9pk@virginia.edu>
2021-11-24 13:57:33 -0800

    finalize storage saves and add todos

--- planets/src/Homepage.js
+++ planets/src/Homepage.js
@@ -2,50 +2,58 @@ import { useEffect } from 'react';
 
 function Homepage() { 
 
+    // call the API in useEffect
     // passing an empty array as the second argument
     // means this is only called on an initial render
+    // for each planet of interest, save data to local storage
+    // so it can be accessed by the description pages
     useEffect(() => {
         fetch('https://api.le-systeme-solaire.net/rest/bodies')
         .then(results => results.json())
         .then(data => {
             for (const val in data['bodies']) {
+                // TODO: per planet, add the code to populate the DOM w/ its info
+                // add something like, planet name, some short description, then a link out to the description page (once that's setup)
+                // could encapsulate in a table (like HW1) 
+                // or have them all arranged horizontally (in a row)
+                // possibly add an image/icon per planet as well
                 if (data['bodies'][val].id === 'mercure') {
                     const m = data['bodies'][val];
                     localStorage.setItem("mercury", JSON.stringify(m));
-                    document.getElementById("merc").innerHTML += m.id;
+                    // example
+                    document.getElementById("merc").innerHTML += m.englishName;
                 }
                 if (data['bodies'][val].id === 'venus') {
                     const v = data['bodies'][val];
                     localStorage.setItem("venus", JSON.stringify(v));
-                    document.getElementById("venus").innerHTML += v.id;
                 }
                 if (data['bodies'][val].id === 'terre') {
                     const e = data['bodies'][val];
-                    // setEarth(e);
+                    localStorage.setItem("earth", JSON.stringify(e));
                 }
                 if (data['bodies'][val].id === 'mars') {
                     const m = data['bodies'][val];
-                    // setMars(m);
+                    localStorage.setItem("mars", JSON.stringify(m));
                 }
                 if (data['bodies'][val].id === 'jupiter') {
                     const j = data['bodies'][val];
-                    // setJupiter(j);
+                    localStorage.setItem("jupiter", JSON.stringify(j));
                 }
                 if (data['bodies'][val].id === 'saturne') {
                     const s = data['bodies'][val];
-                    // setSaturn(s);
+                    localStorage.setItem("saturn", JSON.stringify(s));
                 }
                 if (data['bodies'][val].id === 'uranus') {
                     const u = data['bodies'][val];
-                    // setUranus(u);
+                    localStorage.setItem("uranus", JSON.stringify(u));
                 }
                 if (data['bodies'][val].id === 'neptune') {
                     const n = data['bodies'][val];
-                    // setNeptune(n);
+                    localStorage.setItem("neptune", JSON.stringify(n));
                 }
                 if (data['bodies'][val].id === 'pluton') {
                     const p = data['bodies'][val];
-                    // setPluto(p);
+                    localStorage.setItem("pluton", JSON.stringify(p));
                 }
             }
         })
@@ -58,7 +66,6 @@ function Homepage() {
             </div>
             <div className = "homepage_planets_test">
                 <p id = "merc"></p>
-                <p id = "venus"></p>
             </div>
         </div>
     )
